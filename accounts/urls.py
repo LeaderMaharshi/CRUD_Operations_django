37f75b9a2bd9abe2c2a9eac3b60bfa9a6991c418
LeaderMaharshi <leadermaharshi@gmail.com>
2023-02-01 19:53:26 +0530

    Login and Signup templates added

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.accounts, name="accounts")
+    path('', views.accounts, name="accounts"),
+    path('login/', views.login , name="login"),
+    path('signup/', views.signup , name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -3,3 +3,10 @@ from django.shortcuts import render
 # Create your views here.
 def accounts(request):
     return render(request, 'crud.html')
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def signup(request):
+    return render(request, 'signup.html')
