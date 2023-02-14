ac02967e8285cb07f2cfa2d506c9b6a2ff3ff62e
LeaderMaharshi <leadermaharshi@gmail.com>
2023-02-14 19:28:28 +0530

    Form active

--- products/models.py
+++ products/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Product(models.Model):
     name = models.CharField(max_length=500, unique=True)
-    price = models.FloatField()
+    price = models.FloatField(null = True)
     image = models.ImageField(upload_to='photos/products')
 
     def __str__(self):
--- products/views.py
+++ products/views.py
@@ -1,11 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from . forms import ProductForm
 
 # Create your views here.
 
 def crud(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        price = request.POST.get('price')
+        image = request.POST.get('image')
+        product = Product(
+            name = name,
+            price = price,
+            image = image,
+        )
+        product.save()
+        return redirect('crud')
+        
     items = Product.objects.all()
     context = {
         'items': items,
+        'form': ProductForm,
     }
     return render(request, 'crud.html', context)
